44909f5a381862ce293de355685e9d4e2b0ab34b
Catherine Harrison <mrs.wizrd@gmail.com>
2019-12-11 17:19:02 +0000

    Updates

---
+++ precache-manifest.2ab23b58e03cd21d7f402db55f1ef3f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1719f4042f9501b20427a0834ff19ac",
+    "url": "/decrement-counter/index.html"
+  },
+  {
+    "revision": "b9b37d409d42bd270c45",
+    "url": "/decrement-counter/static/css/main.cfe60efb.chunk.css"
+  },
+  {
+    "revision": "b46ddce8907fde79c455",
+    "url": "/decrement-counter/static/js/2.cb9c59fd.chunk.js"
+  },
+  {
+    "revision": "b9b37d409d42bd270c45",
+    "url": "/decrement-counter/static/js/main.f8ebc1ae.chunk.js"
+  },
+  {
+    "revision": "0835875527b4641866d5",
+    "url": "/decrement-counter/static/js/runtime-main.0ba99f58.js"
+  }
+]);
